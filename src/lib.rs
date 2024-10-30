e6ab4baec33c57880b772df73a5f1741e4d7c1e6
rajmani7584 <rajmani7584@gmail.com>
2024-10-30 22:17:11 +0530

    intial commit

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,59 @@ mod chromeos_update_engine;
 #[no_mangle]
 pub extern "system" fn Java_com_rajmani7584_payloaddumper_MainActivity_getPartitionList<'local>(mut env: JNIEnv<'local>, _class: JClass<'local>, path: JString<'local>) -> jstring {
 
-    let mut payload = Payload::new(env.get_string(&path).expect("err msg").into());
-    let result = payload.init();
-    let res = result.unwrap().to_string();
-    let msg = env.new_string(res).expect("error msg 2").into_raw();
+    let mut msg: String = Default::default();
+
+    let mut payload = match Payload::new(env.get_string(&path).expect("Err: msg").into()) {
+        Ok(p) => {
+            p
+        }
+        Err(err) => {
+            return env.new_string(format!("Err:{}", err)).expect("Err:expect").into_raw();
+        }
+    };
+    
+    let _ = match payload.get_partition_list() {
+        Ok(res) => {
+            msg.insert_str(msg.len(), &res);
+        }
+        Err(err) => {
+            return env.new_string(format!("Err:{}", err)).expect("Err:expect").into_raw();
+        }
+    };
+
+    let msg = env.new_string(msg).expect("Err:expect").into_raw();
 
     return msg;
+}
+
+
+#[no_mangle]
+pub extern "system" fn Java_com_rajmani7584_payloaddumper_MainActivity_extractPartition<'local>(mut env: JNIEnv<'local>, _class: JClass<'local>, path: JString<'local>, partition: JString<'local>, out_path: JString<'local>) -> jstring {
+    
+    let mut msg: String = Default::default();
+
+    let path: String = env.get_string(&path).expect("Err:expect").into();
+
+    let mut payload = match Payload::new(path) {
+        Ok(p) => {
+            p
+        }
+        Err(err) => {
+            return env.new_string(format!("Err:{}", err)).expect("Err:expect").into_raw();
+        }
+    };
+
+    let partition: String = env.get_string(&partition).expect("Err:expect").into();
+    let out: String = env.get_string(&out_path).expect("Err:expect").into();
+
+    let _ = match payload.extract(&partition, &out) {
+        Ok(_res) => {
+            msg.insert_str(msg.len(), "Done");
+        }
+        Err(err) => {
+            return env.new_string(format!("Err:{}", err)).expect("Err:expect").into_raw();
+        }
+    };
+
+    return env.new_string(msg).expect("Err:expect").into_raw();
 }
--- src/payload.rs
+++ src/payload.rs
@@ -1,101 +1,211 @@
-use std::fs::File;
-use std::io::Read;
-use byte_unit::Byte;
-use std::error::Error;
+use std::{error::Error, fs::File, io::{self, BufReader, Read, Seek, SeekFrom}, str};
+use bzip2::read::BzDecoder;
+use xz2::read::XzDecoder;
 
-use crate::chromeos_update_engine;
-
-use crate::chromeos_update_engine::DeltaArchiveManifest;
-use crate::chromeos_update_engine::Signatures;
-
-// struct Request {
-//     partition: chromeos_update_engine::PartitionUpdate,  // Assuming proper binding for chromeos_update_engine
-//     target_directory: String,
-// }
-struct PayloadHeader {
-    version: u64,
-    manifest_len: u64,
-    metadata_signature_len: u32,
-    size: u64
-}
+use crate::chromeos_update_engine::{install_operation::Type, DeltaArchiveManifest, PartitionUpdate};
 
 const PAYLOAD_HEADER_MAGIC: &str = "CrAU";
 const BRILLO_MAJOR_PAYLOAD_VERSION: u64 = 2;
-// const BLOCK_SIZE: u64 = 4096;
-
+const BLOCK_SIZE: u64 = 4096;
 
 pub struct Payload {
-    filename: String,
-    file: Option<File>,  // Rust’s Option is used to handle None cases instead of nil
+    path: String,
+    file: File,
     header: Option<PayloadHeader>,
-    delta_archive_manifest: Option<chromeos_update_engine::DeltaArchiveManifest>,  // Assuming protobuf is used
-    signatures: Option<chromeos_update_engine::Signatures>,
+    manifest: Option<DeltaArchiveManifest>,
+}
 
-    // concurrency: usize,
-    metadata_size: i64,
-    data_offset: i64,
-    initialized: bool,
-
-    // requests: Arc<Mutex<Vec<Request>>>,  // Using thread-safe types for concurrency
-  }
+pub struct PayloadHeader {
+    version: u64,
+    size: u64,
+    manifest_len: u64,
+    signature_len: u32,
+    data_offset: u64,
+    metadata_size: u64
+}
 
 impl Payload {
-    pub fn new(filename: String) -> Payload {
-        Payload {
-            filename,
-            file: None,
+    pub fn new(path: String) -> Result<Payload, Box<dyn Error>> {
+        let file = match File::open(path.clone()) {
+            Ok(f) => f,
+            Err(err) => {
+                return Err(format!("Err:{}", err).into());
+            }
+        };
+        Ok(Payload {
+            path,
+            file,
             header: None,
-            delta_archive_manifest: None,
-            signatures: None,
-            // concurrency: 4,
-            metadata_size: 0,
-            data_offset: 0,
-            initialized: false,
-            // requests: Arc::new(Mutex::new(vec![]))
-        }
+            manifest: None,
+        })
     }
 
-    pub fn init(&mut self) -> Result<String, Box<dyn Error>> {
-        self.open()?;  // Open the payload file
-        let mut msg: String = Default::default();
+    fn init(&mut self) -> Result<(), Box<dyn Error>> {
+        match self.read_header() {
+            Ok(header) => self.header = Some(header),
+            Err(err) => {
+                return Err(err);
+            }
+        }
 
+        match self.read_manifest() {
+            Ok(manifest) => self.manifest = Some(manifest),
+            Err(err) => {
+                return Err(err);
+            }
+        }
+        Ok(())
+    }
+
+    fn read_header(&mut self) -> Result<PayloadHeader, Box<dyn Error>> {
+        let mut buf = [0; 4];
+
+        self.file.read_exact(&mut buf)?;
+
+        if str::from_utf8(&buf)? != PAYLOAD_HEADER_MAGIC {
+            return Err("Invalid Payload magic".into());
+        }
         let mut header = PayloadHeader {
             version: 0,
             manifest_len: 0,
-            metadata_signature_len: 0,
-            size: 0
+            signature_len: 0,
+            size: 0,
+            data_offset: 0,
+            metadata_size: 0
         };
 
+        let mut buf = [0; 8];
+        self.file.read_exact(&mut buf)?;
+        header.version = u64::from_be_bytes(buf);
 
-        header.read_from_payload(self)?;  // Read the header
+        if header.version != BRILLO_MAJOR_PAYLOAD_VERSION {
+            return Err("Unsupported payload version".into());
+        }
 
+        let mut buf = [0; 8];
+        self.file.read_exact(&mut buf)?;
+        header.manifest_len = u64::from_be_bytes(buf);
 
-        self.header = Some(header);
-        // Read the manifest
-        self.read_manifest()?;
-        // Read metadata signature
-        self.read_metadata_signature()?;
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        header.signature_len = u32::from_be_bytes(buf);
 
-        self.metadata_size = (self.header.as_ref().unwrap().size + self.header.as_ref().unwrap().manifest_len) as i64;
+        header.size = 24;
+        header.metadata_size = header.size + header.manifest_len;
+        header.data_offset = header.signature_len as u64 + header.metadata_size;
 
-        self.data_offset = self.metadata_size + self.header.as_ref().unwrap().metadata_signature_len as i64;
+        Ok(header)
+    }
 
+    fn read_manifest(&mut self) -> Result<DeltaArchiveManifest, Box<dyn Error>> {
+        let manifest_len = self.header.as_ref().unwrap().manifest_len as usize;
+        let mut manifest_buf = vec![0; manifest_len];
 
+        self.file.read_exact(&mut manifest_buf)?;
 
-        if let Some(manifest) = &self.delta_archive_manifest {
+        let delta_manifest: DeltaArchiveManifest = prost::Message::decode(&manifest_buf[..])?;
+
+        Ok(delta_manifest)
+    }
+
+    pub fn extract(&mut self, partition_to_extract: &str, out_file: &str) -> Result<String, Box<dyn Error>> {
+        if let Err(err) = self.init() {
+            return Err(err);
+        }
+
+        if let Some(manifest) = &self.manifest {
+            let mut partition: Option<&PartitionUpdate> = None;
+            let partitions = manifest.partitions.clone();
+            for (_, p) in partitions.iter().enumerate() {
+                if partition_to_extract == p.partition_name {
+                    partition = Some(p);
+                    if let Err(err) = self.extract_selected(p, out_file) {
+                        return Err(err);
+                    }
+                };
+            }
+            if partition.is_none() {
+                return Err(format!("partition: {} not found in {}", partition_to_extract, &self.path).into());
+            }
+        }
+
+        Ok("Done".into())
+    }
+
+    fn extract_selected(&mut self, partition: &PartitionUpdate, out_file: &str) -> Result<(), Box<dyn Error>> {
+        let mut output_file = match File::create(out_file) {
+            Ok(f) => {
+                f
+            }
+            Err(err) => {
+                return Err(format!("file create error: {}", err).into());
+            }
+        };
+        let name = &partition.partition_name;
+        // let info = partition.new_partition_info.as_ref().unwrap();
+        // let total_operations = partition.operations.len() as u64;
+        // let size = info.size.as_ref().unwrap();
+
+        let mut reader = BufReader::new(&self.file);
+
+        for operation in &partition.operations {
+            if operation.dst_extents.is_empty() {
+                return Err(format!("invalid dstextents for partition: {}", name).into());
+            }
+
+            let dst = operation.dst_extents[0];
+            let data_offset = operation.data_offset.unwrap() + self.header.as_ref().unwrap().data_offset;
+            let data_length = operation.data_length.unwrap();
+            let expected_uncompress_block_size = dst.num_blocks() * BLOCK_SIZE;
+
+            let _ = reader.seek(SeekFrom::Start(data_offset));
+            let mut reader = Read::take(&mut reader, data_length);
+
+            match operation.r#type() {
+                Type::Replace => {
+                    let _ = io::copy(&mut reader, &mut output_file);
+                },
+                Type::ReplaceXz => {
+                    let mut decoder = XzDecoder::new(reader);
+                    let _ = io::copy(&mut decoder, &mut output_file);
+                },
+                Type::ReplaceBz => {
+                    let mut decoder = BzDecoder::new(reader);
+                    let _ = io::copy(&mut decoder, &mut output_file);
+                },
+                Type::Zero => {
+                    let mut filler = io::repeat(0).take(expected_uncompress_block_size);
+                    let n = io::copy(&mut filler, &mut output_file)?;
+                    if n != expected_uncompress_block_size {
+                        return Err(format!("Err:writing zero: {}", name).into())
+                    }
+                },
+                _ => {
+                    return Err(format!("Unsupported operation type: {}", operation.r#type).into());
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn get_partition_list(&mut self) -> Result<String, Box<dyn Error>> {
+
+        let mut msg: String = Default::default();
+
+        if let Err(err) = self.init() {
+            return Err(err);
+        }
+        
+        if let Some(manifest) = &self.manifest {
            
-            msg.insert_str(msg.len(), "Found partitions:");
+            msg.insert_str(msg.len(), "Partitions:");
 
             for (i, partition) in manifest.partitions.iter().enumerate() {
                 let partition_name = &partition.partition_name;
                 let partition_size = partition.new_partition_info.as_ref().map_or(0, |info| info.size.expect("info size not found"));
 
-                let human_readable_size = Byte::from_u128(partition_size as u128)
-                    .expect("Byte error")
-                    .get_appropriate_unit(byte_unit::UnitType::Decimal)
-                    .to_string();
-
-                    let mg = format!("{}, ({})", partition_name, human_readable_size);
+                let mg = format!("{}|{},", partition_name, partition_size);
                 msg.insert_str(msg.len(), mg.as_str());
 
                 if i < manifest.partitions.len() - 1 {
@@ -105,107 +215,8 @@ impl Payload {
                 }
             }
         } else {
-            println!("No partitions found.");
+            msg = String::from("No partitions found");
         }
-
-        self.initialized = true;
         Ok(msg)
-    }
-
-
-    // fn read_data_blob(&self, offset: i64, length: i64) -> Result<Vec<u8>, Box<dyn Error>> {
-    //     let mut buf = vec![0u8; length as usize];  // Create a buffer of the specified length
-    //     let mut file = self.file.as_ref().ok_or("File is not opened")?; // Access the file from Payload
-
-    //     // Move to the data offset and read from there
-    //     file.seek(SeekFrom::Start((self.data_offset + offset) as u64))?;
-    //     let n = file.read(&mut buf)?;
-
-    //     // Ensure the number of bytes read matches the expected length
-    //     if n as i64 != length {
-    //         return Err(format!("Read length mismatch: {} != {}", n, length).into());
-    //     }
-
-    //     Ok(buf)  // Return the buffer if everything is successful
-    // }
-
-    // fn set_concurrency(&mut self, concurrency: usize) {
-    //     self.concurrency = concurrency;
-    // }
-
-    // fn get_concurrency(&self) -> usize {
-    //     self.concurrency
-    // }
-
-    fn open(&mut self) -> Result<(), Box<dyn Error>> {
-        let file = File::open(&self.filename)?;
-        self.file = Some(file);
-        Ok(())
-    }
-
-    fn read_manifest(&mut self) -> Result<(), Box<dyn Error>> {
-        let manifest_len = self.header.as_ref().unwrap().manifest_len as usize;
-        let mut manifest_buf = vec![0; manifest_len];
-
-        self.file.as_mut().unwrap().read_exact(&mut manifest_buf)?;
-
-        let manifest: DeltaArchiveManifest = prost::Message::decode(&manifest_buf[..])?;
-        self.delta_archive_manifest = Some(manifest);
-
-        Ok(())
-    }
-
-    fn read_metadata_signature(&mut self) -> Result<(), Box<dyn Error>> {
-        let metadata_signature_len = self.header.as_ref().unwrap().metadata_signature_len as usize;
-        if metadata_signature_len == 0 {
-            return Ok(());
-        }
-
-        let mut sig_buf = vec![0; metadata_signature_len];
-        self.file.as_mut().unwrap().read_exact(&mut sig_buf)?;
-
-        let signatures: Signatures = prost::Message::decode(&sig_buf[..])?;
-        self.signatures = Some(signatures);
-
-        Ok(())
-    }
-
-}
-
-
-impl PayloadHeader {
-    fn read_from_payload(&mut self, payload: &mut Payload) -> Result<(), Box<dyn Error>> {
-        let mut buf = [0; 4];
-        payload.file.as_mut().unwrap().read_exact(&mut buf)?;
-
-        if std::str::from_utf8(&buf)? != PAYLOAD_HEADER_MAGIC {
-            return Err("Invalid payload magic".into());
-        }
-
-        // Read Version
-        let mut buf = [0; 8];
-        payload.file.as_mut().unwrap().read_exact(&mut buf)?;
-        self.version = u64::from_be_bytes(buf);
-        println!("Payload Version: {}", self.version);
-
-        if self.version != BRILLO_MAJOR_PAYLOAD_VERSION {
-            return Err("Unsupported payload version".into());
-        }
-
-        // Read Manifest Len
-        let mut buf = [0; 8];
-        payload.file.as_mut().unwrap().read_exact(&mut buf)?;
-        self.manifest_len = u64::from_be_bytes(buf);
-        println!("Payload Manifest Length: {}", self.manifest_len);
-
-        self.size = 24;
-
-        // Read Manifest Signature Length
-        let mut buf = [0; 4];
-        payload.file.as_mut().unwrap().read_exact(&mut buf)?;
-        self.metadata_signature_len = u32::from_be_bytes(buf);
-        println!("Payload Manifest Signature Length: {}", self.metadata_signature_len);
-
-        Ok(())
     }
 }
